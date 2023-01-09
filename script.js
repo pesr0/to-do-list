7dc45fbe7f19cfebdf2b725af5b38baa61d9ba87
pedroT <pedrotmartin00@hotmail.com>
2023-01-09 19:07:46 -0300

    arrumando script para adicionar containers e ajustado um pousco do style

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-window.onload = function () {
-    
+const main = document.querySelector('#caixas');    //main
+
+    var i=0;
+
     function adiciona () {
-    const div =document.createElement('div');
+        var div =document.createElement('div');            //container caixa de texto  
+        var inp = document.createElement('input');        //botao dentro do container
+        var ol = document.createElement('ol');             //lista do container
 
-        div.style.boxSizing = 'border-box';
-        div.style.margin = '0.9%';
-        div.style.background= 'white';
-        div.style.width= '267px';
-        div.style.height= '45%';
-        div.style.border = '1px solid black';
+        div.className = 'caixa';
+            
+        inp.className = 'input_texto'
+        inp.placeholder = 'Digite um novo item aqui'
+        ol.className= 'lista';
 
-const caixas = document.querySelector('#caixas');
-caixas.appendChild(div);
+        ol.appendChild(inp);
+        div.appendChild(ol);
+        main.appendChild(div);
+
+        
     }
-
 document.querySelector('.botao-m').addEventListener('click', adiciona);
+
+
+ var input_texto = document.querySelector('.input_texto');
+ var caixa = document.querySelector('.caixa');
+ var lista = document.querySelector('.lista');
+
+
+if (input_texto==''){
+input_texto.style.margin ='auto';
 }

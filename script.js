cd9f0d5dced0977981bca5ab2a2f98092c94adbe
pedroT <pedrotmartin00@hotmail.com>
2023-01-09 16:53:59 -0300

    adcionado containers para caixas de texto

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function teste () {
-    alert();
+window.onload = function () {
+    
+    function adiciona () {
+    const div =document.createElement('div');
+
+        div.style.boxSizing = 'border-box';
+        div.style.margin = '0.9%';
+        div.style.background= 'white';
+        div.style.width= '267px';
+        div.style.height= '45%';
+        div.style.border = '1px solid black';
+
+const caixas = document.querySelector('#caixas');
+caixas.appendChild(div);
     }
 
-document.querySelector('.botao').addEventListener('click', teste);
+document.querySelector('.botao-m').addEventListener('click', adiciona);
+}

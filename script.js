d719cc3d4d94e2e56db2cf2746dfa7851ceff9a5
pedroT <pedrotmartin00@hotmail.com>
2023-01-10 16:09:42 -0300

    seções de listas adicionadas com js funcionando e checkbox adicionado sem css

--- script.js
+++ script.js
@@ -1,69 +1,105 @@
 const main = document.querySelector('#boxes');
- 
+
 //add + new box of inputs and a new title input
-function adiciona () {
-       const div =document.createElement('div');
-       div.className = 'box'; 
-       main.appendChild(div);
+function adiciona ()
+    {
+        const div =document.createElement('div');
+        div.className = 'box'; 
+        main.appendChild(div);
 
-       const ol = document.createElement('ol');
-       ol.className= 'mainlist';
-       div.appendChild(ol);
+        const ol = document.createElement('ol');
+        ol.className= 'mainlist';
+        div.appendChild(ol);
 
-       const li = document.createElement('li');
-       ol.appendChild(li);
+        const li = document.createElement('li');
+        li.className= 'list_fItem';
+        ol.appendChild(li);
 
-       const inp = document.createElement('input');
+        const inp = document.createElement('input');
         inp.className = 'input_text input_title';
         inp.placeholder = 'Please insert this section title';
         li.appendChild(inp);
         inp.focus();
 
         //add + new inputs inside te box os inputs
-        function add_input (event) {
-           // console.log(ol.children[ol.childElementCount])
+        function add_input (event)
+        {
+            //count how many empty inputs exists in the list
             let j = 0;
-            for(let i = 0; i < ol.childElementCount; i++){
-                if (ol.children[i].value==='')j++;
+            for(let i = 0; i < ol.childElementCount; i++)
+            {
+                if (ol.children[i].children[0].value==='')j++;
             }
 
-            if(event.keyCode==8){
-            if (j>1){
-                for (let i=0; i<ol.childElementCount; i++){
-                    if(ol.children[i].value===''&& ol.children[i]!=ol.children[ol.childElementCount-1]) ol.removeChild(ol.children[i]);
-                }
-            }
-            for (let i=0; i<ol.childElementCount; i++){
-                if(j==1 && ol.children[i].value===''&& ol.children[ol.childElementCount-1]!='') {
-                ol.removeChild(ol.children[i]);
-                const new_inp = document.createElement('input');
-                const li_1 = document.createElement('li');
-                new_inp.placeholder='Digite um novo item aqui';
-                new_inp.className='input_text';
-                ol.appendChild(li_1);
-                li_1.appendChild(new_inp);
-            }
-            }
-        }
+            //when backspace is clicked, remove empty inputs when the last input isn't empty, and add a new bottom empty input
+            if(event.keyCode === 8)
+            {
+                for (let i=0; i<ol.childElementCount; i++)
+                {
+                    if(ol.children[i].children[0].value===''&& ol.children[ol.childElementCount-1].children[0]!='')
+                    {
+                        ol.removeChild(ol.children[i]);
+    
+                        if(j>1) return;
 
-            if (event.keyCode != 13) {console.log('enter nao pressionado'); return;}    
-            for(let i = 0; i < ol.childElementCount; i++){
-                    if (ol.children[i].value==='') {console.log('string vazia encontrada'); return;}
+                        if(j===1)
+                        {
+                            const new_li = document.createElement('li');
+                            new_li.className = 'list_item';
+                            ol.appendChild(new_li);
+
+                            const new_inp = document.createElement('input');
+                            new_inp.placeholder='Insert a new item here';
+                            new_inp.className='input_text';
+                            new_li.appendChild(new_inp);
+                            
+                            const chk = document.createElement('input');
+                            chk.type = 'checkbox';
+                            chk.className = 'check';
+                            new_li.appendChild(chk);
+
+                            new_inp.addEventListener('keydown', add_input);
+                            return;
+                        }
+                    }
                 }
-               
+            }
+
+            //if keydown is "Enter" a new input is added if there are no empty inputs
+            if (event.keyCode === 13) 
+            {
+                for(let i = 0; i < ol.childElementCount; i++)
+                {
+                    //Verifies if there is another empty input
+                    if (ol.children[i].children[0].value==='') 
+                    {
+                        console.log('string vazia encontrada'); return;
+                    }
+                }
+
+                //If there are no empty inputs anymore, one new is created
+                const new_li = document.createElement('li');
+                new_li.className= 'list_item';
+                ol.appendChild(new_li);
+
                 const new_inp = document.createElement('input');
-                const li_2 = document.createElement('l1');
-                new_inp.placeholder='Insert a ney item';
+                new_inp.placeholder='Insert a new item here';
                 new_inp.className='input_text';
-                ol.appendChild(li_2);
-                li_2.appendChild(new_inp);
+                new_li.appendChild(new_inp);
+
+                const chk = document.createElement('input');
+                chk.type = 'checkbox';
+                chk.className = 'check';
+                new_li.appendChild(chk);
+
                 new_inp.focus();
                 new_inp.addEventListener('keydown', add_input);
             }
-
-            inp.addEventListener('keydown', add_input);
-    
         }
-        
+
+        inp.addEventListener('keydown', add_input);
     
+    }
+
+
 document.querySelector('.button-m').addEventListener('click', adiciona);

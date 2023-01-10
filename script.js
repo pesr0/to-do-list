77acd2fe7a5a5f1485bb0f8afc1dc437f4adfbf7
pedroT <pedrotmartin00@hotmail.com>
2023-01-10 00:09:55 -0300

    adcionado funcionamento basico de itens de lista e alguma estilizacao

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const main = document.querySelector('#caixas');    //main
+ 
 
-    var i=0;
-
-    function adiciona () {
-        var div =document.createElement('div');            //container caixa de texto  
-        var inp = document.createElement('input');        //botao dentro do container
-        var ol = document.createElement('ol');             //lista do container
-
+function adiciona () {
+       var div =document.createElement('div');
+       var inp = document.createElement('input');
+       var ol = document.createElement('ol');
+        
         div.className = 'caixa';
+        div.style.overflow = 'auto';
             
         inp.className = 'input_texto'
         inp.placeholder = 'Digite um novo item aqui'
@@ -16,17 +16,24 @@ const main = document.querySelector('#caixas');    //main
         ol.appendChild(inp);
         div.appendChild(ol);
         main.appendChild(div);
+        inp.focus();
 
+        function adiciona_input (event) {
+            if (event.keyCode != 13) {console.log('enter nao pressionado'); return;}    
+            for(let i = 0; i < ol.childElementCount; i++){
+                    if (ol.children[i].value==='') {console.log('string vazia encontrada'); return;}
+                }
+               
+                let novo_inp = document.createElement('input');
+                novo_inp.placeholder='Digite um novo item aqui';
+                novo_inp.className='input_texto';
+                ol.appendChild(novo_inp);
+                novo_inp.focus();
+                novo_inp.addEventListener('keydown', adiciona_input);
+            }
+
+            inp.addEventListener('keydown', adiciona_input);
         
     }
+
 document.querySelector('.botao-m').addEventListener('click', adiciona);
-
-
- var input_texto = document.querySelector('.input_texto');
- var caixa = document.querySelector('.caixa');
- var lista = document.querySelector('.lista');
-
-
-if (input_texto==''){
-input_texto.style.margin ='auto';
-}

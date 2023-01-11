0dee94b0021cb7bf92c6161a1eec9b87ce5eff68
pedroT <pedrotmartin00@hotmail.com>
2023-01-11 16:06:17 -0300

    js implementada funcionando

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const main = document.querySelector('#boxes');
 
 //add + new box of inputs and a new title input
-function adiciona ()
-    {
-        const div =document.createElement('div');
-        div.className = 'box'; 
-        main.appendChild(div);
+function add_main (){
+
+        const input_box =document.createElement('div');
+        input_box.className = 'box'; 
+        main.appendChild(input_box);
 
         const ol = document.createElement('ol');
         ol.className= 'mainlist';
-        div.appendChild(ol);
+        input_box.appendChild(ol);
 
         const li = document.createElement('li');
         li.className= 'list_fItem';
@@ -22,28 +22,32 @@ function adiciona ()
         inp.focus();
 
         //add + new inputs inside te box os inputs
-        function add_input (event)
-        {
+        function add_input (event){
+            
             //count how many empty inputs exists in the list
             let j = 0;
-            for(let i = 0; i < ol.childElementCount; i++)
-            {
+            for(let i = 0; i < ol.childElementCount; i++){
+
                 if (ol.children[i].children[0].value==='')j++;
             }
 
             //when backspace is clicked, remove empty inputs when the last input isn't empty, and add a new bottom empty input
-            if(event.keyCode === 8)
-            {
-                for (let i=0; i<ol.childElementCount; i++)
-                {
-                    if(ol.children[i].children[0].value===''&& ol.children[ol.childElementCount-1].children[0]!='')
-                    {
+           if(event.keyCode === 8){
+
+                for (let i=0; i<ol.childElementCount; i++){
+
+                    //avaliate is there are some empty input, if the empty input isn't the last one, its removed
+                    if(ol.children[i].children[0].value===''&& ol.children[ol.childElementCount-1].children[0]!=''&&ol.children[i].children[0].className!='input_text input_title'){
+
                         ol.removeChild(ol.children[i]);
     
+                        // j>1 means that there were more than 1 empty inputs, than, with all removed (unless the bottom) the function is done
                         if(j>1) return;
 
-                        if(j===1)
-                        {
+                        //j===1 means that it was only one input which was empty, and by that it means that the bottom input is filled 
+                        //so this section add an empty input in the bottom
+                        if(j===1){
+
                             const new_li = document.createElement('li');
                             new_li.className = 'list_item';
                             ol.appendChild(new_li);
@@ -58,7 +62,14 @@ function adiciona ()
                             chk.className = 'check';
                             new_li.appendChild(chk);
 
+                            chk.addEventListener('change', chk_detect);
                             new_inp.addEventListener('keydown', add_input);
+                            
+                            //detect if the checkbox is marked, if it is the text in the input get a "line-through" decoration, if its not, none decoration
+                            function chk_detect(){
+                                if (chk.checked) new_inp.style.textDecoration='line-through';
+                                else new_inp.style.textDecoration='none';
+                            }
                             return;
                         }
                     }
@@ -66,14 +77,14 @@ function adiciona ()
             }
 
             //if keydown is "Enter" a new input is added if there are no empty inputs
-            if (event.keyCode === 13) 
-            {
-                for(let i = 0; i < ol.childElementCount; i++)
-                {
+            if (event.keyCode === 13){
+
+                for(let i = 0; i < ol.childElementCount; i++){
+
                     //Verifies if there is another empty input
-                    if (ol.children[i].children[0].value==='') 
-                    {
-                        console.log('string vazia encontrada'); return;
+                    if (ol.children[i].children[0].value===''){
+
+                        console.log('empty string found'); return;
                     }
                 }
 
@@ -92,14 +103,20 @@ function adiciona ()
                 chk.className = 'check';
                 new_li.appendChild(chk);
 
+                chk.addEventListener('change', chk_detect);
                 new_inp.focus();
                 new_inp.addEventListener('keydown', add_input);
+
+                //the same checkbox check detect function as above
+                function chk_detect(){
+                    if (chk.checked) new_inp.style.textDecoration='line-through';
+                    else new_inp.style.textDecoration='none';
+                }
             }
         }
 
         inp.addEventListener('keydown', add_input);
-    
     }
 
 
-document.querySelector('.button-m').addEventListener('click', adiciona);
+document.querySelector('.button-m').addEventListener('click', add_main);
